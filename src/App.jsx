54a65e99cdd718309efa15383bae725c8aa1ff0b
hironmoy-ray <hpray35@gmail.com>
2024-10-16 19:30:24 +0600

    Converting HTML Template into React Components

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import Footer from "./components/Footer";
+import Header from "./components/header";
+import HeroSection from "./components/HeroSection";
+import TaskBoard from "./components/task/TaskBoard";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <h1>Task Management App</h1>
+      <Header />
+      <div className="flex flex-col justify-center items-center">
+        <HeroSection />
+        <TaskBoard />
+      </div>
+      <Footer />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
